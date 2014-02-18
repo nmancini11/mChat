c70f318393c0c984661e3f0666b0330bb05d1863
Nick AMancini <nmancini11@gmail.com>
2014-02-17 20:51:47 -0800

    added fonts, styled it a little bit, need to implement auto scroll and previous messages

--- chat.js
+++ chat.js
@@ -1,6 +1,7 @@
 // makes messages available for both the client and the server
 Messages = new Meteor.Collection('messages');
 
+
 if (Meteor.isClient) {
     Template.messages.messages = function () {
         return Messages.find({}, {
@@ -10,19 +11,36 @@ if (Meteor.isClient) {
         });
     };
 
+    //Create global variable for username
+    var userName;
+    var getUsername = function(name) {
+          userName = prompt("Please enter your name");
+
+          if (userName!=null)
+            {
+            x="Hello " + userName + "! Welcome to mChat";
+            $("#welcome").text=x;
+            }
+    }    
+    //Run getUsername function if Meteor.isClient
+    getUsername();
+
     Template.entryfield.events = {
         "keydown #message": function (event) {
             // this line checks to see if the "Enter / Return key was pressed"
             // the enter key has a value of 13
             if (event.which == 13) {
                 //Submit 'ze form please
-                var name = document.getElementById('name');
+                //Figure out how to grab the name on a new session and store it
                 var message = document.getElementById('message');
+                var name = userName;
                 // adjusting time
                 var a_p = "";
                 var time = new Date();
                 var curr_hour = time.getHours();
+                var curr_min = time.getMinutes();
                 var curr_seconds = time.getSeconds();
+
 
                 if (curr_hour < 12)
                    {
@@ -41,18 +59,18 @@ if (Meteor.isClient) {
                    curr_hour = curr_hour - 12;
                    }
 
-                var curr_min = time.getMinutes();
+                // make the time pretty so that people can read it
                 var displayTime = curr_hour + ":" + ('0' + curr_min).slice(-2) + ":" + ('0' + curr_seconds).slice(-2) + " " + a_p;
-                console.log(displayTime);
 
-                if (name.value !== '' && message.value !== '') {
+                // insert message to DB as long as message.value is not blank
+                if (message.value !== '') {
                     Messages.insert({
-                        name: name.value,
+                        name: userName,
                         message: message.value,
                         time: displayTime
                     });
                     
-                    name.value = '';
+                    // name.value = '';
                     message.value = '';
                 }
             }
